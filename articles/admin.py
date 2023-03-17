32a126c6978d1ffa203144da7ab6c50a1b1fb512
erinbedri <erinbedri@gmail.com>
2023-03-17 11:09:25 +0100

    registered articles to admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from articles.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Article, ArticleAdmin)
+
