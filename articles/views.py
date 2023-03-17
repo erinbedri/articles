cfa9128eb742e82039129d27a862fa7f33c475a8
erinbedri <erinbedri@gmail.com>
2023-03-17 12:05:20 +0100

    articles ordered by date

--- articles/views.py
+++ articles/views.py
@@ -6,6 +6,7 @@ from articles.models import Article
 
 class ArticleListView(ListView):
     model = Article
+    queryset = Article.objects.all().order_by('-created_at')
 
 
 class ArticleDetailView(DetailView):
